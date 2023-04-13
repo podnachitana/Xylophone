15e5e84b21c6b0100ccee74e48d43c700be75bff
podnachitana <t.zudina1@gmail.com>
2023-04-13 15:41:17 +0300

    Delete comments

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,10 +20,8 @@ class ViewController: UIViewController {
 	@IBAction func keyPressed(_ sender: UIButton) {
 		playSound(soundName: (sender.titleLabel?.text)!)
 		
-		//Reduces the sender's (the button that got pressed) opacity to half.
 		sender.alpha = 0.5
 		
-		//Code should execute after 0.2 second delay.
 		DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
 			//Bring's sender's opacity back up to fully opaque.
 			sender.alpha = 1.0
